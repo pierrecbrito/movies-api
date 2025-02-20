494df36143bd7a69ec316ba6a9bfe561b74dc9ae
Pierre Brito <33200451+pierrecbrito@users.noreply.github.com>
2025-02-19 21:15:30 -0300

    router setup and test

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,10 @@ import config from 'config';
 const app = express();
 app.use(express.json());
 
-const PORT = config.get<number>('port');
+import router from './router';
+app.use('/api/', router);
 
+const PORT = config.get<number>('port');
 app.listen(PORT, async () => {
     console.log(`Server is running on port ${PORT}`);
 });
---
+++ src/router.ts
@@ -0,0 +1,6 @@
+import {Router, Request, Response} from 'express';
+const router = Router();
+
+export default router.get('/test', (req: Request, res: Response) => {
+    res.status(200).json({message: 'Hello, world!'});
+});
