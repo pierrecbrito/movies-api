9ca6a8009d8b4c380a86d13bdf05941468ee3087
Pierre Brito <33200451+pierrecbrito@users.noreply.github.com>
2025-02-19 21:01:59 -0300

    server setup

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,9 @@
-console.log('Hello World');
+import express from 'express';
+import config from 'config';
+
+const app = express();
+app.use(express.json());
+
+app.listen(3000, async () => {
+    console.log(`Server is running on port 3000`);
+});
