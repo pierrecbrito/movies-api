607c1a9f61f78c613236e7cf2a97ba7bcd165d29
Pierre Brito <33200451+pierrecbrito@users.noreply.github.com>
2025-02-20 22:02:18 -0300

    Creating instance of movie on createMovie

--- src/controllers/movieControllers.ts
+++ src/controllers/movieControllers.ts
@@ -4,5 +4,12 @@ import Logger from '../../config/logger';
 
 
 export  async function createMovie(req: Request, res: Response) {
-    res.status(200).send("Deu certo");
+    try {
+        const data = req.body
+        const movie = await MovieModel.create(data);
+        res.status(201).json(movie);
+    } catch (error) {
+        Logger.error(error);
+        res.status(500).json({message: 'Internal server error'});
+    }
 }
